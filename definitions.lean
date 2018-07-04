7cde9f551de1272ac9a1291ef66218d13fcd631d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-07-04 11:33:19 +0100

    Definition of simple loong endgame

---
+++ definitions.lean
@@ -0,0 +1,11 @@
+import data.multiset
+
+structure simple_loony_endgame :=
+(three_chains : ℕ) -- number of three-chains
+(four_loops : ℕ)
+(six_loops : ℕ)
+(long_chains : multiset ℕ)
+(long_chains_are_long : ∀ x ∈ long_chains, x ≥ 4)
+(long_loops : multiset ℕ)
+(long_loops_are_long : ∀ x ∈ long_loops, x ≥ 8)
+(long_loops_are_even : ∀ x ∈ long_loops, 2 ∣ x)
