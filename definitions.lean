304bad7d54d96677d7af26fd04169cbe5999ba6b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-07-05 14:46:35 +0100

    adding empty game

--- definitions.lean
+++ definitions.lean
@@ -1,5 +1,4 @@
-import data.multiset
-
+@[derive decidable_eq]
 structure simple_loony_endgame :=
 (three_chains : ℕ) -- number of three-chains
 (four_loops : ℕ)
@@ -9,3 +8,14 @@ structure simple_loony_endgame :=
 (long_loops : multiset ℕ)
 (long_loops_are_long : ∀ x ∈ long_loops, x ≥ 8)
 (long_loops_are_even : ∀ x ∈ long_loops, 2 ∣ x)
+
+definition empty_game : simple_loony_endgame :=
+{ three_chains := 0,
+  four_loops := 0,
+  six_loops := 0,
+  long_chains := multiset.zero,
+  long_chains_are_long := λ x Hx, false.elim Hx,
+  long_loops := {},
+  long_loops_are_long := λ x Hx, false.elim Hx,
+  long_loops_are_even := λ x Hx, false.elim Hx 
+}
