4ca58ddf873d9a3ac9fb23bcd2b290ac6899eb08
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-07-14 13:35:56 +0100

    computing value of one chain  / one loop

--- src/kb_value.lean
+++ src/kb_value.lean
@@ -1,29 +1,62 @@
 import extended_N_le tactic.ring
 
--- Given a multiset s of chain lengths (each of which need to be >= 3 for this
--- function to make sense) this returns the value (to the person whose move it is not)
--- of the game.
-
-def chain_value (s0 : multiset ℕ) : ℕ := 
-multiset.strong_induction_on s0 $ λ s H,multiset.N_min $ multiset.pmap
-  (λ (a : ℕ) (h : a ∈ s),a - 2 + int.nat_abs (2 - H (s.erase a) (multiset.erase_lt.2 h))) s (λ a, id)
-
-#eval (chain_value {4,5,6}) -- 7
-#eval chain_value {3,3,3,3,3,3,3,3}
-
-def loop_value (s0 : multiset ℕ) : ℕ := multiset.strong_induction_on s0 $ λ s H,multiset.N_min $ multiset.pmap
-  (λ (a : ℕ) (h : a ∈ s),a - 4 + int.nat_abs (4 - H (s.erase a) (multiset.erase_lt.2 h))) s (λ a, id)
-
-#eval loop_value {4,4,4,4}
-
+-- simplified version of SLE (because I want easy access to the multisets of loops and chains)
 @[derive decidable_eq]
-structure sle' :=
+structure sle :=
 (long_chains : multiset ℕ)
 (long_chains_are_long : ∀ x ∈ long_chains, x ≥ 3)
 (long_loops : multiset ℕ)
 (long_loops_are_long_and_even : ∀ x ∈ long_loops, x ≥ 4 ∧ 2 ∣ x)
 
-definition value_aux (C : multiset ℕ) : multiset ℕ → ℕ := 
+
+definition empty_game : sle := {long_chains := ∅,long_chains_are_long := dec_trivial, 
+  long_loops := ∅, long_loops_are_long_and_even := dec_trivial}
+
+definition example_game : sle :=
+{ long_chains := {3,3,3,3,8},
+  long_chains_are_long := dec_trivial,
+  long_loops := {4},
+  long_loops_are_long_and_even := dec_trivial
+}
+
+-- note: the 20 lines below are just WARM-UP -- it's KB practising for the real definitions.
+
+-- Given a multiset s of chain lengths (each of which need to be >= 3 for this
+-- function to make sense) this returns the value (to the person whose move it is not)
+-- of the game.
+
+/-
+def chain_value (s0 : multiset ℕ) : ℕ := 
+multiset.strong_induction_on s0 $ 
+  λ s H,multiset.N_min $ multiset.pmap
+    (λ (a : ℕ) (h : a ∈ s),a - 2 + int.nat_abs (2 - H (s.erase a) (multiset.erase_lt.2 h))) 
+    s 
+    (λ a, id)
+
+--#eval (chain_value {4,5,6}) -- 7
+--#eval chain_value {3,3,3,3,3,3,3,3} -- 2
+
+def loop_value (s0 : multiset ℕ) : ℕ := multiset.strong_induction_on s0 $ λ s H,multiset.N_min $ multiset.pmap
+  (λ (a : ℕ) (h : a ∈ s),a - 4 + int.nat_abs (4 - H (s.erase a) (multiset.erase_lt.2 h))) s (λ a, id)
+
+--#eval loop_value {4,4,4,4}
+-/
+-- Here is some technical induction thing:
+
+open multiset 
+
+/-
+
+-- equation lemma for multiset 
+
+theorem multiset.strong_induction_eq {α : Type} {p : multiset α → Sort*}
+  (s : multiset α) (H) : @strong_induction_on _ p s H =
+    H s (λ t h, @strong_induction_on _ p t H) :=
+by rw [strong_induction_on]
+
+-- this is the real definition of value, using strong induction on multisets.
+
+definition value_aux_orig (C : multiset ℕ) : multiset ℕ → ℕ := 
   multiset.strong_induction_on C (λ C2 HC L,(
     multiset.strong_induction_on L (λ L2 HL,
       multiset.N_min (multiset.pmap 
@@ -34,23 +67,87 @@ definition value_aux (C : multiset ℕ) : multiset ℕ → ℕ :=
     )
   ))
 
-definition G0 : sle' :=
-{ long_chains := {3,3,3,3,8},
-  long_chains_are_long := dec_trivial,
-  long_loops := {4},
-  long_loops_are_long_and_even := dec_trivial
-}
+-- I couldn't prove the equation lemma with this defintion.
+-/
 
-definition value (G : sle') := value_aux G.long_chains G.long_loops
+-- This should be the same definition, built using the equation compiler.
+
+def value_aux : multiset ℕ → multiset ℕ → ℕ
+| C L := multiset.N_min (multiset.pmap 
+      (λ a (h : a ∈ C), 
+        have multiset.card (C.erase a) < multiset.card C,
+          from multiset.card_lt_of_lt (multiset.erase_lt.2 h),
+--        have multiset.card (C.erase a) + multiset.card L < multiset.card C + multiset.card L, 
+--          from add_lt_add_right (multiset.card_lt_of_lt (multiset.erase_lt.2 h)) _,
+        a - 2 + int.nat_abs (2 - value_aux (C.erase a) L))
+        C (λ _,id) + multiset.pmap 
+      (λ a (h : a ∈ L), 
+        have multiset.card (L.erase a) < multiset.card L,
+          from multiset.card_lt_of_lt (multiset.erase_lt.2 h),
+--        have multiset.card C + multiset.card (multiset.erase L a) < multiset.card C + multiset.card L, 
+--          from add_lt_add_left (multiset.card_lt_of_lt (multiset.erase_lt.2 h)) _,
+        a - 4 +int.nat_abs (4 - value_aux C (L.erase a)))
+        L (λ _,id))
+using_well_founded {rel_tac := λ _ _, `[exact ⟨_, measure_wf 
+  (λ CL, multiset.card CL.fst + multiset.card CL.snd)⟩]}
+
+/- These definitions have been shoehorned into Lean, and for both of them 
+   we now need to prove the key property, namely
+-/
+
+/-
+lemma value_aux_orig_eq (C L : multiset ℕ) : 
+value_aux C L = multiset.N_min 
+  (multiset.pmap
+    (λ a (h : a ∈ C), 
+      a - 2 + int.nat_abs (2 - value_aux_orig (C.erase a) L))
+        C (λ _,id) 
+  + multiset.pmap 
+    (λ a (h : a ∈ L), 
+      a - 4 +int.nat_abs (4 - value_aux_orig C (L.erase a)))
+        L (λ _,id)
+  ) :=
+begin
+  show ((multiset.strong_induction_on C (λ C2 HC L,(
+    multiset.strong_induction_on L (λ L2 HL,
+      multiset.N_min (multiset.pmap 
+      (λ a (h : a ∈ C2), a - 2 + int.nat_abs (2 - HC (C2.erase a) (multiset.erase_lt.2 h) L2))
+      C2 (λ _,id) + multiset.pmap 
+      (λ a (h : a ∈ L2), a - 4 +int.nat_abs (4 - HL (L2.erase a) (multiset.erase_lt.2 h)))
+      L2 (λ _,id))
+    )
+  ))) : multiset ℕ → ℕ) L = _,
+  rw multiset.strong_induction_eq C,
+  dsimp,
+  rw multiset.strong_induction_eq L,
+  dsimp,
+  congr,funext,congr,
+  sorry
+end 
+-/
+
+lemma value_aux_eq (C L : multiset ℕ) : 
+value_aux C L = multiset.N_min 
+  (multiset.pmap
+    (λ a (h : a ∈ C), 
+      a - 2 + int.nat_abs (2 - value_aux (C.erase a) L))
+        C (λ _,id) 
+  + multiset.pmap 
+    (λ a (h : a ∈ L), 
+      a - 4 +int.nat_abs (4 - value_aux C (L.erase a)))
+        L (λ _,id)
+  ) := value_aux._main.equations._eqn_1 C L 
+
+definition value (G : sle) := value_aux G.long_chains G.long_loops
 
 definition size_aux (C L : multiset ℕ) := multiset.card C + multiset.card L 
 
-definition size (G : sle') : ℕ := size_aux G.long_chains G.long_loops
+definition size (G : sle) : ℕ := size_aux G.long_chains G.long_loops
 
 definition fcv_aux (C L : multiset ℕ) : ℤ := ↑(multiset.sum C + multiset.sum L) 
   - 4 * multiset.card C - 8 * multiset.card L
 
-definition fcv (G : sle') : ℤ := fcv_aux G.long_chains G.long_loops
+definition fcv (G : sle) : ℤ := fcv_aux G.long_chains G.long_loops
 
 -- Chris and Simon decidability thing
 instance decidable_exists_multiset {α : Type*} (s : multiset α) (p : α → Prop) [decidable_pred p] :
@@ -62,25 +159,24 @@ suffices this : decidable (∃ a ∈ C, a ≥ 4),
 by { resetI, apply @decidable_of_iff _ _ _ this, apply exists_congr, intro, tauto },
 by { apply_instance }
 
+-- without those 6 lines of gobble-de-gook above, the below lines don't work
 definition tb_aux (C L : multiset ℕ) : ℕ := if (C = 0 ∧ L = 0) then 0 else
   if L = 0 then 4 else
   if C = 0 then 8 else
   if ∃ a : ℕ, a ≥ 4 ∧ a ∈ C then 4 else 
   6
 
-definition tb (G : sle') : ℕ := tb_aux G.long_chains G.long_loops
+definition tb (G : sle) : ℕ := tb_aux G.long_chains G.long_loops
 
 definition cv_aux (C L : multiset ℕ) : ℤ := fcv_aux C L + tb_aux C L
-definition cv (G : sle') : ℤ := fcv G + tb G
+
+definition cv (G : sle) : ℤ := fcv G + tb G
 
 open nat 
 
-definition empty_game : sle' := {long_chains := ∅,long_chains_are_long := dec_trivial, 
-  long_loops := ∅, long_loops_are_long_and_even := dec_trivial}
-
 lemma cv_zero : cv empty_game = 0 := dec_trivial 
 
-definition single_chain (c : ℕ) (Hc : c ≥ 3) : sle' :=
+definition single_chain (c : ℕ) (Hc : c ≥ 3) : sle :=
 { long_chains := {c},
   long_chains_are_long := λ x H, begin
   rwa multiset.mem_singleton.1 H,
@@ -92,10 +188,12 @@ definition single_chain (c : ℕ) (Hc : c ≥ 3) : sle' :=
 @[simp] lemma multiset.sum_singleton {α : Type} [add_comm_monoid α]
 (c : α) : multiset.sum (c :: 0) = c := begin rw multiset.sum_cons c 0,exact add_zero c end
 
+/-
 @[simp] lemma multiset.sum_singleton' {α : Type} [add_comm_monoid α]
 (c : α) : multiset.sum {c} = c := multiset.sum_singleton c 
+-/
 
-lemma cv_one_chain (c : ℕ) : cv_aux {c} 0 = c :=
+lemma cv_one_chain (c : ℕ) : cv_aux (c :: 0) 0 = c :=
 begin
   unfold cv_aux tb_aux,
   split_ifs,
@@ -105,7 +203,7 @@ begin
   simp,ring 
 end 
 
-lemma cv_one_loop (l : ℕ) : cv_aux 0 {l} = l :=
+lemma cv_one_loop (l : ℕ) : cv_aux 0 (l :: 0) = l :=
 begin
   unfold cv_aux tb_aux,
   split_ifs,
@@ -116,39 +214,41 @@ begin
   simp,ring
 end
 
-open multiset 
-#check @strong_induction_on 
-#print prefix multiset.strong_induction_on 
+@[simp] lemma v_empty : value_aux 0 0 = 0 := by {rw value_aux_eq;simp}
 
-theorem strong_induction_eq {α : Type} {p : multiset α → Sort*}
-  (s : multiset α) (H) : @strong_induction_on _ p s H =
-    H s (λ t h, @strong_induction_on _ p t H) :=
-by rw [strong_induction_on]
+/-
+lemma multiset.pmap_singleton {α : Type} {β : Type} (c : α) (p : α → Prop) 
+  (f : α → β) (h : ∀ a : α, a ∈ (c :: 0) → p a) : 
+multiset.pmap (λ (a : α) (h₂ : p a), f a) (c :: 0) h = (f c :: 0) := by simp
+-/
 
-@[simp] lemma v_empty : value_aux 0 0 = 0 :=
-begin
-  unfold value_aux, -- strong_induction hell
-  rw strong_induction_eq, -- goal now one page long
-  rw strong_induction_eq, -- goal now two pages long
-  simp,
-end 
-
--- I don't have the tools for this yet
 lemma v_one_chain (c : ℕ) (h : c ≥ 3) : value_aux (c :: 0) 0 = c :=
 begin
-  unfold value_aux,
-  rw strong_induction_eq,
-  rw strong_induction_eq,
-  simp,
-  rw strong_induction_eq,
-  rw strong_induction_eq,
-  simp,
+  rw value_aux_eq,
+  rw pmap_zero,
+  rw add_zero,
+  suffices : int.nat_abs 2 + (c - 2) = c,
+  simp [this],
   show 2 + (c - 2) = c,
   rw add_comm,refine nat.sub_add_cancel _,
   exact le_trans (show 2 ≤ 3, by exact dec_trivial) h,
 end
 
 -- we are getting somewhere!
+
+lemma v_one_loop (l : ℕ) (h : l ≥ 4) : value_aux 0 (l :: 0) = l :=
+begin
+  rw value_aux_eq,
+  rw pmap_zero,
+  rw zero_add,
+  suffices : int.nat_abs 4 + (l - 4) = l,
+  simp [this],
+  show 4 + (l - 4) = l,
+  rw add_comm, refine nat.sub_add_cancel _,
+  assumption
+end 
+
+-- TODO : controlled values
 
 lemma sum_one {a b : ℕ} : a + b = 1 → (a = 0 ∧ b = 1) ∨ (a = 1 ∧ b = 0) :=
 begin
@@ -161,7 +261,7 @@ begin
   exfalso,exact nat.no_confusion (nat.succ_inj H),
 end 
 
-lemma one_comp_case (G : sle') : ((size G) = 1) → (cv G = value G) := 
+lemma one_comp_case (G : sle) : ((size G) = 1) → (cv G = value G) := 
 begin
   intro H,
   have H₂ := sum_one H,
@@ -192,14 +292,14 @@ lemma three_point_one (G : simple_loony_endgame) : ((size_G G) > 0) → (G.three
 
 /-
 definition multiset.sum {α : Type} [has_add α] [has_zero α] (s : multiset α) : α := multiset.fold (+) 0 s 
-def fcv_G (G : sle') := 
+def fcv_G (G : sle) := 
 simple_loony_endgame.three_chains G + simple_loony_endgame.four_loops G 
 + simple_loony_endgame.six_loops G + multiset.card (simple_loony_endgame.long_loops G) 
 + multiset.card (simple_loony_endgame.long_chains G) 
 - (multiset.card (multiset.erase_dup (simple_loony_endgame.long_chains G)) + 1)*4 
 - (multiset.card (multiset.erase_dup (simple_loony_endgame.long_loops G)) + 2)*8 
 
-lemma one_comp_case (G : sle') : (size G) = 1) → (cv_G G = v_G G) := sorry
+lemma one_comp_case (G : sle) : (size G) = 1) → (cv_G G = v_G G) := sorry
 
 
 #eval value G -- gives 0, which looks right
