064d2f0cc54623a1cdf40cf914e065c191ae82ff
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-08-23 12:51:06 +0100

    notes on induction

--- src/ellen_induction.lean
+++ src/ellen_induction.lean
@@ -1,5 +1,12 @@
 import Dotsandboxes__Part1
 
+/- my proposal for an inductive hypothesis-/
+theorem sle.induction_on (p : sle → Sort*)
+(H0 : p empty_game_sle)
+(Hchain : ∀ (G : sle) (c : ℕ) (Hc : c ≥ 3), p G → p (cons_chain_to_sle G c Hc))
+(Hloop : ∀ (G : sle) (l : ℕ) (Hl : l ≥ 4) (Pl : 2 ∣ l), p G → p (cons_loop_to_sle G l Hl Pl)) :
+∀ G : sle, p G := sorry 
+
 theorem two_point_two_ak_bk_loop_aux (G : sle) (a : nat) (h : a ∈ G.loops) (k : ℕ) :
 size G = k → 
 (cv G ≥ 2) → (size G ≥ 2) → ¬ ((G.chains = (3 :: 0)) ∧ (multiset.card(G.loops) = 1))
