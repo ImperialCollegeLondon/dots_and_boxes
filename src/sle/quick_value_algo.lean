f1638708721fed44d919f6eb82ecebdac3411f5f
Ellen <ellen@null>
2019-10-29 16:19:44 +0000

    quick_value_aux

--- src/sle/quick_value_algo.lean
+++ src/sle/quick_value_algo.lean
@@ -3,5 +3,28 @@ import extended_N_le tactic.ring
 import sle.basic
 
 
-definition quick_value_algo (G : sle) :=  value G
+
+/-- auxiliary function used to define value -/
+def quick_value_aux : multiset ℕ → multiset ℕ → ℕ
+| C L := multiset.N_min (multiset.pmap 
+      (λ a (h : a ∈ C)(p : ∀ c : C, a ≤ c), 
+        have multiset.card (C.erase a) < multiset.card C,
+          from multiset.card_lt_of_lt (multiset.erase_lt.2 h),
+--        have multiset.card (C.erase a) + multiset.card L < multiset.card C + multiset.card L, 
+--          from add_lt_add_right (multiset.card_lt_of_lt (multiset.erase_lt.2 h)) _,
+         a - 2 + int.nat_abs (2 - value_aux (C.erase a) L))
+        C (λ _,id) + multiset.pmap 
+      (λ a (h : a ∈ L)(p : ∀ l : L, a ≤ l), 
+        have multiset.card (L.erase a) < multiset.card L,
+          from multiset.card_lt_of_lt (multiset.erase_lt.2 h),
+--        have multiset.card C + multiset.card (multiset.erase L a) < multiset.card C + multiset.card L, 
+--          from add_lt_add_left (multiset.card_lt_of_lt (multiset.erase_lt.2 h)) _,
+         a - 4 +int.nat_abs (4 - value_aux C (L.erase a)))
+        L (λ _,id))
+using_well_founded {rel_tac := λ _ _, `[exact ⟨_, measure_wf 
+  (λ CL, multiset.card CL.fst + multiset.card CL.snd)⟩]}
+
+
+definition quick_value_algo (G : sle) :=  
+/-if 
  
